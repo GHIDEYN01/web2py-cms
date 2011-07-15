62e6b011d331bffa12b20c046ec4644c40dfc8e7
Gilson Filho <contatogilsonsbf@gmail.com>
2011-07-15 23:25:04 +0000

    Refactoring controller and inserting comments.

--- controllers/default.py
+++ controllers/default.py
@@ -19,18 +19,26 @@ along with web2py-cms.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 def index():
+    """List all posts from the most current."""
     posts = t_post._db(t_post.id>0).select(orderby=~t_post.datetime)
 
     return dict(posts = posts)
 
+
 def post():
+    """Display the full post."""
     post = get_post(args = request.args) or redirect(URL_INDEX_PAGE)
     t_post_comment.post.default = post.id
+    
     if auth.is_logged_in():
         t_post_comment.commented_by.default = auth.user_id
     crud_post_comment.settings.formstyle='divs'
+    
     if config.email_new_comments:
-        crud_post_comment.settings.create_onaccept = lambda form_c:mail.send(to=config.admin_email, subject=T('New comment on post %s')%post.title, message='Comment: %s '%form_c.vars.comment)
+        crud_post_comment.settings.create_onaccept = lambda form_c:mail.send(
+            to=config.admin_email, subject=T('New comment on post %s') % post.title,
+            message='Comment: %s '%form_c.vars.comment)
+            
     form_new_comment = crud_post_comment.create(t_post_comment)
     comments = post.post_comment.select()
 
@@ -38,10 +46,12 @@ def post():
 
 
 def contact():
+    """Display the page contact form"""
     if not config.enable_contact_page:
         session.flash('Ops! contact page is not enabled :(')
         redirect(URL_INDEX_PAGE)
     else:
+        # Contact Form
         form = FORM(TABLE(
             TR(
                 TD(T('Your name:')),
@@ -60,9 +70,12 @@ def contact():
                 TD(INPUT(_type='submit', _value=T('Send'))))))
 
         if form.accepts(request.vars, session):
-            data = dict(app = BLOG_NAME, name = form.vars.name, email = form.vars.email, message = form.vars.message)
-            message = T('Hello!\n\n%(name)s (email: %(email)s) sent a message to you, through the contact page of'+ \
+            data = dict(app = BLOG_NAME, name = form.vars.name, 
+                email = form.vars.email, message = form.vars.message)
+            message = T('Hello!\n\n%(name)s (email: %(email)s) sent a message to you,'+\
+                        'through the contact page of'+ \
                         ' the site %(app)s\nmessage:%(message)s')%data
+                        
             if mail.send(to=CONTACT_EMAIL, subject='Contact message %s'%BLOG_NAME,message=message):
                 session.flash=T('Thank you, message sent.')
                 redirect(URL_INDEX_PAGE)
@@ -71,7 +84,9 @@ def contact():
 
     return dict(form = form)
 
+
 def user():
+    """Register user"""
     if not t_user._db(t_user.id>0).select().first():
         auth.settings.register_onaccept.append(lambda auth_form:__give_permission(role='admin', user_id=auth_form.vars.id))
         auth.settings.register_onaccept.append(lambda auth_form:__give_permission(role='author', user_id=auth_form.vars.id))
@@ -81,6 +96,7 @@ def user():
 
 @auth.requires_membership('admin')
 def manage():
+    """Listing all users in the system."""
     arg = request.args(0)
     if arg == 'users':
         authors = None #TODO select usuários com papel author
@@ -98,6 +114,7 @@ def dashboard():
 
 @auth.requires_membership('admin')
 def customize():
+    """Change config system"""
     form_config = crud_config.update(t_config, config, deletable  = False)
     menus = t_menu._db(t_menu.id>0).select()
     pages = t_page._db(t_page.id>0).select()
@@ -106,7 +123,7 @@ def customize():
 
 @auth.requires_membership('author')
 def new():
-
+    """Create post"""
     t_post.posted_by.readable = False
     form = crud_post.create(t_post, onaccept=lambda form:set_permalink(form.vars.id))
 
@@ -115,6 +132,7 @@ def new():
 
 @auth.requires_membership('author')
 def edit():
+    """Update post"""
     arg = request.args(0)
     if arg == 'post':
         post = get_post(args = request.args[1:])    
@@ -137,6 +155,7 @@ def edit():
 
 @auth.requires_membership('author')
 def delete():
+    """Delete post"""
     arg = request.args(0) or redirect(URL_INDEX_PAGE)
     if arg == 'comment':
         comment = t_post_comment[request.args(1)] or redirect(URL_INDEX_PAGE)
@@ -149,6 +168,7 @@ def delete():
 
 @auth.requires_membership('admin')
 def menu():
+    """Manage menus in the system"""
     arg = request.args(0) or redirect(URL_INDEX_PAGE)
     response.view = '%s/%s.%s'%('default', arg, request.extension)
     if arg == 'new':
@@ -173,6 +193,7 @@ def menu():
 
 @auth.requires_membership('admin')
 def page():
+    """Manage pages in the system"""
     arg = request.args(0) or redirect(URL_INDEX_PAGE)
     response.view = '%s/%s.%s'%('default', arg, request.extension)
 
@@ -203,6 +224,7 @@ def page():
 
 
 def pages():
+    """View the page"""
     page = t_page._db(t_page.url == '/%s'%request.args(0)).select().first() or redirect(URL_INDEX_PAGE)
     if not page.use_default_layout:
         response.view = '%s/%s.%s'%('default', 'pages_unstyled', request.extension)
